8118997397c8f92f564cd5045065ba05a9ee83f8
Evan Marmer <evanmarmer@gmail.com>
2023-12-16 13:20:40 -0700

    make /create-goal feature, add axios

--- public/main.js
+++ public/main.js
@@ -1,1 +1,23 @@
-console.log('hello')
+let form = document.getElementById('create-form');
+let goal = document.getElementById('create-goal');
+let needed = document.getElementById('create-needed');
+let date = document.getElementById('create-date');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let maBod = {
+        goal: goal.value,
+        needed: needed.value,
+        finished: date.value
+    }
+
+    axios.post('/create-goal', maBod)
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+})
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import { fileURLToPath } from 'url'
 
 let app = express()
 
+app.use(express.json())
+
+let db = []
+
 app.get('/', (req, res) => {
     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
 })
@@ -16,7 +20,10 @@ app.get('/js', (req, res) => {
     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/main.js'))
 })
 
-
+app.post('/create-goal', (req, res) => {
+    db.push(req.body)
+    res.status(200).send(db)
+})
 
 app.listen(8090, () => {
     console.log('we started on port 8090!')
